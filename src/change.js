3d0a8f279b202dd768d6b3438f6718daf0b765fb
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2019-10-30 13:06:10 -0400

    added change.js

--- src/change.js
+++ src/change.js
@@ -6,9 +6,17 @@ const MAX = 0.99;
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
-    // print your error message here
+    console.log("Invalid")
 } else if (amount < MIN || amount > MAX) {
-    // print your error message here
+    console.log("Invalid")
 } else {
-    // write your code for making change here
+    const Q = .25
+    const D = .10
+    const N = .5
+    const P = .1
+    const divq = 0.99 / Q
+    const divd = 0.99 / D
+    const divn = 0.99 / N
+    const divp = 0.99 / P
 }
+console.log("\n" + divq + "quarters, " + divd + "dimes, " + divn + "nickels, " + divp + "pennies.")
