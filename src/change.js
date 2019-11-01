83d2a986a1aa3ef014f5adf4a5aa9a405ec575bd
Sarah Cadet <SCadet1@ucvts.tec.nj.us>
2019-11-01 12:38:28 -0400

    Change

--- src/change.js
+++ src/change.js
@@ -3,6 +3,10 @@ const readlineSync = require("readline-sync");
 const MIN = 0.01;
 const MAX = 0.99;
 
+const Q = 25;
+const D = 10;
+const N = 5;
+
 const amount = Number(readlineSync.question("\nEnter a dollar amount: "));
 
 if (Number.isNaN(amount)) {
@@ -10,13 +14,14 @@ if (Number.isNaN(amount)) {
 } else if (amount < MIN || amount > MAX) {
     console.log("Invalid")
 } else {
-    const Q = .25
-    const D = .10
-    const N = .5
-    const P = .1
-    const divq = 0.99 / Q
-    const divd = 0.99 / D
-    const divn = 0.99 / N
-    const divp = 0.99 / P
-}
-console.log("\n" + divq + "quarters, " + divd + "dimes, " + divn + "nickels, " + divp + "pennies.")
+    let pennies = Math.round(amount * 100);
+
+    let quarters = Math.floor(pennies / Q);
+    pennies = pennies % Q;
+    let dimes = Math.floor(pennies / D);
+    pennies = pennies % D;
+    let nickels = Math.floor(pennies / N);
+    pennies = pennies % N;
+
+    console.log("\n" + quarters + " quarters, " + dimes + " dimes, " + nickels + " nickels, " + pennies + " pennies. ")
+  }
